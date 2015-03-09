e9e6268dd01ae66dfe917d922e0bf77fe8f4e389
Robert Warneford-Thomson <warnefordthomsonr@gmail.com>
2015-03-09 12:45:35 -0400

    Added functions to normalize embryo movies by raw blot intensity.
    Function Blotscale.R takes embryo blot data and normalizes it to one arbitrary reference embryo, outputting a data.frame with the slope of each embryo's blot values versus the reference.
    
    Function BlotApply.R takes these normalization factors and divides each embryos blot values by its respective factor to output normalized blot values.

---
+++ Blotscale.R
@@ -0,0 +1,13 @@
+# Compares cell averaged embryo blot data against arbitrary reference
+# embryo and generates scaling factor scales blot data to reference embryo
+Blotscale <- function(AverageDF) {
+    df <- data.frame(Embryo = names(AverageDF)[-c(1)])
+
+    for (i in 1:length(df$Embryo))  
+    { temp.norm <-lm(AverageDF[[2]] ~ AverageDF[[i+1]])
+      df$slope[[i]] <- coef(temp.norm)[2] }
+
+return(df)}
+
+
+
--- Cell_Timeline.R
+++ Cell_Timeline.R
@@ -1,6 +1,5 @@
 # Produce cell aggregated expression data plotted by chronological order.
 
-
 # generate list of cell names
 CellID <- levels(Data$SCD_blot$cell)
 
@@ -11,6 +10,8 @@ Data[["EmbOr_NormalizedZ_blot"]] <-  lapply(EmbOrientationRL,
        function(lst) {cbind(ID = Data$SCD_blot$cell, Time = Data$SCD_blot$cellTime, data.frame(lapply(EmbAxisData[[lst]], 
        function(df) { Znorm <- (33-df$zraw)*Zcorr + 1
         df$blot*Znorm})))}) 
+        rm(Zcorr)
+
 names(Data$EmbOr_NormalizedZ_blot) <- EmbOrientationRL
 
 # Combine L and R Z-normalized blot data 
@@ -24,6 +25,7 @@ Data$cellblot <- lapply(Data$EmbOr_NormalizedZ_blot, function(df) {aggregate(df[
 # Add first time point to each cell average entry
 CellStartTimes <- unlist(lapply(CellID, function(Cell) {Data$EmbOr_NormalizedZ_blot$Both$Time[Data$EmbOr_NormalizedZ_blot$Both$ID %in% Cell][1]}))
 Data$cellblot <- lapply(Data$cellblot, function(df) {cbind(df[1], Time = CellStartTimes, df[-c(1)])})
+rm(CellStartTimes, CellID)
 names(Data$cellblot) <- EmbOrientationRLB
 
 # summary of cell averaged blot values across all replicates 
@@ -31,4 +33,4 @@ Data$AverageCellblot <- lapply(Data$cellblot, cellDFsummary)
 names(Data$AverageCellblot) <- EmbOrientationRLB
 
 # Subset Averaged blot data for highly-expressing cells and larger CV values
-Data$SortAvCellBlot <- lapply(Data$AverageCellblot, function(x) {Sortblot(df = x, LoMean = 100, LoCV = 0, HiCV = 5)})
+Data$SortAvCellBlot <- lapply(Data$AverageCellblot, function(x) {Sortblot(df = x, LoMean = 100, LoCV = 0,)})
