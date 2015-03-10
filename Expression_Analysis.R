693eeade38ed6d0386660159c937f72ccaa81b58
Robert Warneford-Thomson <warnefordthomsonr@gmail.com>
2015-03-10 13:50:02 -0400

    Changed scripts to functions to allow for modular analysis.
    The following scripts were modified into function form.
    
    Cell_Timeline.R
    trimmingSCD.R
    SCD_statistics.R
    
    SCD_statistics.R is now function GroupEmb, which no longer computes statistics and now only groups embryo data by axis orientation under microscope.
    
    In addition, defunct code from trimmingSCD.R related to gweight normalization was removed for performance

--- Expression_Analysis.R
+++ Expression_Analysis.R
@@ -17,40 +17,42 @@ source('~/Nematode_R/Plotcell.R')
 source('~/Nematode_R/Blotscale.R')
 source('~/Nematode_R/BlotApply.R')
 source('~/Nematode_R/Zscale.R')
+source("trimmingSCD.R")
+source("Cell_Timeline.R")
 
 # Specify raw data source directory
 Rawdir <- "Combined RW10890"
 
 # Import SCD data
-Data <- ImportSCD(Rawdir)
+Data_A <- ImportSCD(Rawdir)
 
 # Generate annotation file for SCD data with auxillary information for desired worm strain
-RepAuxInfo <- RepAuxfiles("RW10890", Rawdir)
+RepAuxInfo_A <- RepAuxfiles("RW10890", Rawdir)
 
 # Trim data
-source("trimmingSCD.R")
+Data_A <- Trimming(Data_A)
 
-# Compute Statistics
-source("SCD_statistics.R")
+# Group Embryos by Orientation
+EmbData_A <- GroupEmb(Data_A, RepAuxInfo_A)
 
 # Screen cells for high gene expression
-source("Cell_Timeline.R")
+EmbData_A <- BlotTime(EmbData_A, EmbData_A)
 
 # Plots dual ordinate plot of Averaged blot data (Mean and CV)
-PlotAllCells(LoCell = 1, HiCell = , df = Data$SortAvCellBlot$Both, sort = "CV",outnames = FALSE)
-names <- PlotAllCells(LoCell = 1, HiCell = 35, df = Data$SortAvCellBlot$Both, sort = "CV",outnames = TRUE)
+PlotAllCells(LoCell = 1, HiCell = , df = EmbData_A$SortAvCellBlot$Both, sort = "CV",outnames = FALSE)
+names <- PlotAllCells(LoCell = 1, HiCell = 35, df = EmbData_A$SortAvCellBlot$Both, sort = "CV",outnames = TRUE)
 names <- as.character(names)
 
 # plots specific cell reporter expression over cell lifetime
-Plotcell("Eal", df =  Data$NormZblot$Both, ancestors = TRUE)
+Plotcell("Eal", df =  EmbData_A$NormZblot$Both, ancestors = TRUE)
 
 # plots entire lineage of reporter expression over time
-Plotcell("Cpppap", df =  Data$NormZblot$Both, ancestors = TRUE)
+Plotcell("Cpppap", df =  EmbData_A$NormZblot$Both, ancestors = TRUE)
 
 # Plot lineages of top 20 expressing cells
 pdf(onefile = TRUE, file = "Combined RW10890 Pal-1 Expression_lowCV.pdf" )
 for (i in 1:length(names))
-{Plotcell(names[i], df = Data$NormZblot$Both, ancestors = TRUE)}
+{Plotcell(names[i], df = EmbData_A$NormZblot$Both, ancestors = TRUE)}
 dev.off()
   rm(i, names, RepAuxInfo)
 
